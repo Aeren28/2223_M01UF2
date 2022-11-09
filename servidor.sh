4b29799ae9b21c660c93ac0424dc436621e8ef1b
nerea <nerea.fernandez@enti.cat>
2022-11-09 11:12:14 +0100

    Comprovaciones y vacas

--- servidor.sh
+++ servidor.sh
@@ -1,13 +1,50 @@
 #!/bin/bash
 
+PORT="2223"
+SERVER_AD="localhost"
+
 echo "Servidor Transfer Unite Recursive International Protocol: TURIP"
 
-echo "(0) LISTEN"
-MSG=`nc -l 2223`
+echo "(0) LISTEN: Handshake"
 
-if [ "$MSG" != "HOLI_TURIP" ]
+MSG=`nc -l $PORT`
+
+HANDSHAKE=`echo $MSG | cut -d " " -f 1`
+IP_CLIENT=`echo $MSG | cut -d " " -f 2`
+
+echo "(3) SEND: Comprobación"
+
+if [ "$HANDSHAKE" != "HOLI_TURIP" ]
 then 
 	echo "ERROR 1: Handshake incorrecto"
+	
+	echo "KO_TURIP" | nc $IP_CLIENT $PORT
+
 	exit 1
 fi
+
+echo "OK_TURIP" | nc $IP_CLIENT $PORT
+
+echo "(4) LISTEN: FILE"
+MSG=`nc -l $PORT`
+
+ARCHIVO=`echo $MSG | cut -d " " -f 1`
+FILE_NAME=`echo $MSG | cut -d " " -f 2`
+
+echo "(7) SEND: Comprobación de FILE"
+
+if [ "$ARCHIVO" != "FILE_NAME" ]
+then
+	echo "ERROR 2: File incorrecto"
+	
+	echo "KO_FILE_NAME" |nc $IP_CLIENT $PORT
+
+	exit 2
+fi
+
+echo "OK_FILE_NAME" | nc $IP_CLIENT $PORT
+
+exit 0
+
+
 
